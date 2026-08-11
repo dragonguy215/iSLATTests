2195f2a4579d68dafb2a64e8738632ac80a09117
Johnny <51426425+John-M87@users.noreply.github.com>
2026-08-11 14:12:14 -0500

    Updated the list of available HITRAN isotopologues The new values will not work until the hitran module of astroquery is updated to include these new values too

--- iSLAT/Modules/Hitran_data.py
+++ iSLAT/Modules/Hitran_data.py
@@ -28,17 +28,16 @@ def get_global_identifier(molecule_name, isotopologue_number=1):
 
     mol_isot_code = molecule_name + '_' + str(isotopologue_number)
 
-    trans = { 'H2O_1':1, 'H2O_2':2, 'H2O_3':3, 'H2O_4':4, 'H2O_5':5, 'H2O_6':6, 'H2O_7':129,
-               'CO2_1':7,'CO2_2':8,'CO2_3':9,'CO2_4':10,'CO2_5':11,'CO2_6':12,'CO2_7':13,'CO2_8':14,
-               'CO2_9':121,'CO2_10':15,'CO2_11':120,'CO2_12':122,
+    trans = {  'H2O_1':1, 'H2O_2':2, 'H2O_3':3, 'H2O_4':4, 'H2O_5':5, 'H2O_6':6, 'H2O_7':129,
+               'CO2_1':7,'CO2_2':8,'CO2_3':9,'CO2_4':10,'CO2_5':11,'CO2_6':12,'CO2_7':13,'CO2_8':14,'CO2_9':121,'CO2_10':15,'CO2_11':120,'CO2_12':122,
                'O3_1':16,'O3_2':17,'O3_3':18,'O3_4':19,'O3_5':20,
                'N2O_1':21,'N2O_2':22,'N2O_3':23,'N2O_4':24,'N2O_5':25,
                'CO_1':26,'CO_2':27,'CO_3':28,'CO_4':29,'CO_5':30,'CO_6':31,
                'CH4_1':32,'CH4_2':33,'CH4_3':34,'CH4_4':35,
                'O2_1':36,'O2_2':37,'O2_3':38,
                'NO_1':39,'NO_2':40,'NO_3':41,
-               'SO2_1':42,'SO2_2':43,
-               'NO2_1':44,
+               'SO2_1':42,'SO2_2':43,'SO2_3':137,'SO2_4':138,
+               'NO2_1':44,'NO2_3':149,'NO2_2':130,
                'NH3_1':45,'NH3_2':46,
                'HNO3_1':47,'HNO3_2':117,
                'OH_1':48,'OH_2':49,'OH_3':50,
@@ -47,7 +46,7 @@ def get_global_identifier(molecule_name, isotopologue_number=1):
                'HBr_1':54,'HBr_2':55,'HBr_3':111,'HBr_4':112,
                'HI_1':56,'HI_2':113,
                'ClO_1':57,'ClO_2':58,
-               'OCS_1':59,'OCS_2':60,'OCS_3':61,'OCS_4':62,'OCS_5':63,
+               'OCS_1':59,'OCS_2':60,'OCS_3':61,'OCS_4':62,'OCS_5':63,'OCS_6':135,
                'H2CO_1':64,'H2CO_2':65,'H2CO_3':66,
                'HOCl_1':67,'HOCl_2':68,
                'N2_1':69,'N2_2':118,
@@ -60,7 +59,7 @@ def get_global_identifier(molecule_name, isotopologue_number=1):
                'COF2_1':80,'COF2_2':119,
                'SF6_1':126,
                'H2S_1':81,'H2S_2':82,'H2S_3':83,
-               'HCOOH_1':84,
+               'HCOOH_1':84, 'HCOOH_2':150,
                'HO2_1':85,
                'O_1':86,
                'ClONO2_1':127,'ClONO2_2':128,
@@ -78,12 +77,18 @@ def get_global_identifier(molecule_name, isotopologue_number=1):
                'SO3_1':114,
                'C2N2_1':123,
                'COCl2_1':124,'COCl2_2':125,
-               'SO_1': 146, 'SO_2': 147,
-               'CH3F_1': 144,
+               'SO_1': 146, 'SO_2': 147, 'SO_3': 148,
+               'CH3F_1': 144, 'CH3F_2': 151,
                'GeH4_1': 139, 'GeH4_2': 140, 'GeH4_3': 141, 'GeH4_4': 142, 'GeH4_5': 143,
                'CS2_1': 131, 'CS2_2': 132, 'CS2_3': 133, 'CS2_4': 134,
                'CH3I_1': 145,
                'NF3_1': 136,
+               'H3+_1': 158,
+               'CH3_1': 159,
+               'S2_1': 152,
+               'COFCl_1': 153, 'COFCl_2': 154,
+               'HONO_1': 157,
+               'ClNO2_1': 155, 'ClNO2_2': 156
                }
  
     return trans[mol_isot_code]
@@ -109,8 +114,10 @@ def get_molecule_identifier(molecule_name):
              '25':'H2O2',  '26':'C2H2', '27':'C2H6',   '28':'PH3',  '29':'COF2', '30':'SF6',  '31':'H2S',   '32':'HCOOH',
              '33':'HO2',   '34':'O',    '35':'ClONO2', '36':'NO+',  '37':'HOBr', '38':'C2H4', '39':'CH3OH', '40':'CH3Br',
              '41':'CH3CN', '42':'CF4',  '43':'C4H2',   '44':'HC3N', '45':'H2',   '46':'CS',   '47':'SO3',   '48':'C2N2',
-            '49':'COCl2',   '50':'SO',   '51':'CH3F',   '52':'GeH4',   '53':'CS2',   '54':'CH3I',   '55':'NF3'}
-    ## Invert the dictionary.                                                                                                          
+             '49':'COCl2', '50':'SO',   '51':'CH3F',   '52':'GeH4', '53':'CS2',  '54':'CH3I', '55':'NF3',
+             '56':'H3+',   '57':'CH3',  '58':'S2',     '59':'COFCl','60':'HONO', '61':'ClNO2',
+             }
+    ## Invert the dictionary.
     trans = {v:k for k,v in trans.items()}
     return(int(trans[molecule_name]))
 
